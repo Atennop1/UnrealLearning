ac7f0049de695a88aedee2a1841a0eed30df44ee
AtennopVer <anton7185569@gmail.com>
2023-09-19 22:55:36 +0300

    Initial commit

---
+++ Source/UnrealLearning/UnrealLearningGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealLearningGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALLEARNING_API AUnrealLearningGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
