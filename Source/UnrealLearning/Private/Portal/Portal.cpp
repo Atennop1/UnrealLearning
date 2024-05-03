87f87de6e48dffca03fb1ffbfaac47ca8b297e7a
AtennopVer <anton7185569@gmail.com>
2024-05-03 23:32:59 +0300

    Made basic portal component

---
+++ Source/UnrealLearning/Private/Portal/Portal.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright Atennop. All Rights Reserved.
+
+#include "Portal/Portal.h"
+#include "Components/SceneCaptureComponent2D.h"
+
+APortal::APortal()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	Capture1 = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("Capture1"));
+	Capture2 = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("Capture2"));
+
+	Capture1->SetupAttachment(Cast<USceneComponent>(GetComponentsByTag(USceneComponent::StaticClass(), "First")[0]));
+	Capture2->SetupAttachment(Cast<USceneComponent>(GetComponentsByTag(USceneComponent::StaticClass(), "Second")[0]));
+}
---
+++ Source/UnrealLearning/Public/Portal/Portal.h
@@ -0,0 +1,22 @@
+﻿// Copyright Atennop. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Portal.generated.h"
+
+UCLASS()
+class UNREALLEARNING_API APortal : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	USceneCaptureComponent2D *Capture1;
+
+	UPROPERTY(EditDefaultsOnly)
+	USceneCaptureComponent2D *Capture2;
+
+public:
+	APortal();
+};
