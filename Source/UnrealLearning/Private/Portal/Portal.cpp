cc7aadf47976142948c74087e3522adcf1661ef7
AtennopVer <anton7185569@gmail.com>
2024-05-04 21:56:32 +0300

    Portals complete remade

--- Source/UnrealLearning/Private/Portal/Portal.cpp
+++ Source/UnrealLearning/Private/Portal/Portal.cpp
@@ -2,13 +2,78 @@
 
 #include "Portal/Portal.h"
 #include "Components/SceneCaptureComponent2D.h"
+#include "Engine/TextureRenderTarget2D.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetRenderingLibrary.h"
 
 APortal::APortal()
 {
-	PrimaryActorTick.bCanEverTick = false;
-	Capture1 = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("Capture1"));
-	Capture2 = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("Capture2"));
+	PrimaryActorTick.bCanEverTick = true;
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	Root->SetupAttachment(RootComponent);
 
-	Capture1->SetupAttachment(Cast<USceneComponent>(GetComponentsByTag(USceneComponent::StaticClass(), "First")[0]));
-	Capture2->SetupAttachment(Cast<USceneComponent>(GetComponentsByTag(USceneComponent::StaticClass(), "Second")[0]));
+	PortalMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PortalMesh"));
+	PortalMesh->SetupAttachment(Root);
+	
+	Capture = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("Capture"));
+	Capture->SetupAttachment(Root);
+}
+
+void APortal::Link(APortal* Portal)
+{
+	if (Portal == nullptr)
+		return;
+
+	Portal->Setup();
+	OtherPortal = Portal;
+	Portal->OtherPortal = this;
+
+	OtherPortal->PortalMaterial->SetTextureParameterValue("Texture", RenderTexture);
+	PortalMaterial->SetTextureParameterValue("Texture", OtherPortal->RenderTexture);
+
+	OtherPortal->Capture->TextureTarget = RenderTexture;
+	Capture->TextureTarget = OtherPortal->RenderTexture;
+}
+
+void APortal::Setup()
+{
+	if (PortalMaterial != nullptr && RenderTexture != nullptr)
+		return;
+	
+	PortalMaterial = UMaterialInstanceDynamic::Create(PortalMesh->GetMaterial(0), this);
+	PortalMesh->SetMaterial(0, PortalMaterial);
+	
+	const FVector2D TextureSize = GEngine->GameViewport->Viewport->GetSizeXY();
+	RenderTexture = UKismetRenderingLibrary::CreateRenderTarget2D(this, TextureSize.X, TextureSize.Y, RTF_RGBA16f);
+}
+
+void APortal::BeginPlay()
+{
+	Super::BeginPlay();
+	SetTickGroup(TG_PostUpdateWork);
+	
+	Setup();
+	Link(OtherPortal);
+}
+
+void APortal::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (OtherPortal == nullptr)
+		return;
+	
+	const FTransform Transform = GetActorTransform();
+	const FVector PortalScale = Transform.GetScale3D();
+	const FTransform OtherTransform = OtherPortal->GetActorTransform();
+	const FTransform CameraTransform = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0)->GetActorTransform();
+	
+	const FVector NewPosition = UKismetMathLibrary::TransformLocation(OtherTransform, UKismetMathLibrary::InverseTransformLocation(FTransform(Transform.GetRotation(), Transform.GetLocation(), FVector(-PortalScale.X, -PortalScale.Y, PortalScale.Z)), CameraTransform.GetLocation()));
+	const FVector NewRotationVectorX = UKismetMathLibrary::TransformDirection(OtherTransform, UKismetMathLibrary::MirrorVectorByNormal(UKismetMathLibrary::MirrorVectorByNormal(UKismetMathLibrary::InverseTransformDirection(Transform, FVector(CameraTransform.GetRotation().X)), FVector(1, 0, 0)), FVector(0, 1,0)));
+	const FVector NewRotationVectorY = UKismetMathLibrary::TransformDirection(OtherTransform, UKismetMathLibrary::MirrorVectorByNormal(UKismetMathLibrary::MirrorVectorByNormal(UKismetMathLibrary::InverseTransformDirection(Transform, FVector(CameraTransform.GetRotation().Y)), FVector(1, 0, 0)), FVector(0, 1,0)));
+	const FVector NewRotationVectorZ = UKismetMathLibrary::TransformDirection(OtherTransform, UKismetMathLibrary::MirrorVectorByNormal(UKismetMathLibrary::MirrorVectorByNormal(UKismetMathLibrary::InverseTransformDirection(Transform, FVector(CameraTransform.GetRotation().Z)), FVector(1, 0, 0)), FVector(0, 1,0)));
+	const FRotator NewRotation = UKismetMathLibrary::MakeRotationFromAxes(NewRotationVectorX, NewRotationVectorY, NewRotationVectorZ);
+	
+	OtherPortal->GetCamera()->SetWorldLocationAndRotation(NewPosition, NewRotation);
 }
--- Source/UnrealLearning/Public/Portal/Portal.h
+++ Source/UnrealLearning/Public/Portal/Portal.h
@@ -11,12 +11,33 @@ class UNREALLEARNING_API APortal : public AActor
 {
 	GENERATED_BODY()
 
+private:
+	UPROPERTY(EditInstanceOnly)
+	APortal *OtherPortal;
+	
 	UPROPERTY(EditDefaultsOnly)
-	USceneCaptureComponent2D *Capture1;
+	USceneComponent *Root;
+	
+	UPROPERTY(EditDefaultsOnly)
+	USceneCaptureComponent2D *Capture;
 
 	UPROPERTY(EditDefaultsOnly)
-	USceneCaptureComponent2D *Capture2;
+	UStaticMeshComponent *PortalMesh;
 
+	UPROPERTY()
+	UMaterialInstanceDynamic *PortalMaterial;
+
+	UPROPERTY()
+	UTextureRenderTarget2D *RenderTexture;
+
+	void Setup();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+	
 public:
 	APortal();
+	void Link(APortal *Portal);
+	USceneCaptureComponent2D *GetCamera() const { return Capture; }
 };
