261fd68bd122f67dc72c55851beca2b0ee6432d9
AtennopVer <anton7185569@gmail.com>
2024-05-15 22:51:19 +0300

    Tested DataTables

---
+++ Source/UnrealLearning/Private/DataTablesTest/ItemsTableViewer.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright Atennop. All Rights Reserved.
+
+#include "DataTablesTest/ItemsTableViewer.h"
+#include "DataTablesTest/ItemData.h"
+
+AItemsTableViewer::AItemsTableViewer()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AItemsTableViewer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<FItemData*> Rows;
+	const FString Context = "DataTableViewerContext";
+	Table->GetAllRows(Context, Rows);
+	
+	for (const auto Row : Rows)
+		GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, Row->Name + ": " + Row->Description);
+}
+
